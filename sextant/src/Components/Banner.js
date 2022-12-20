34d8b182e4420130ed2889f68547a469c2582452
anirudh-20 <59680154+anirudh-20@users.noreply.github.com>
2022-12-20 22:06:00 +0530

    update ui

--- sextant/src/Components/Banner.js
+++ sextant/src/Components/Banner.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
 
 export default function Banner () {
+
+  const [isSmall, setSmall] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const distanceY = window.pageYOffset || document.documentElement.scrollTop;
+      const threshold = 50;
+
+      if (distanceY > threshold) setSmall(true);
+      else setSmall(false);
+      console.log(distanceY, isSmall);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isSmall]);
+
   return(
-    <div className="Banner bg-filter">
-      Dashboard
+    <div className={`Banner bg-filter ` + (isSmall ? 'Small': '')}>
+      Sextant
     </div>
   )
 }
